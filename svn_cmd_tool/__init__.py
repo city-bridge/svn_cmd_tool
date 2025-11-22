2da47cc1f31d4bedb8997e5b6c09778ab81f6f90
city-bridge <hironori0130@gmail.com>
2025-11-22 17:13:42 +0900

    Ver.Up

--- svn_cmd_tool/__init__.py
+++ svn_cmd_tool/__init__.py
@@ -4,7 +4,7 @@ from .svn_checkout_manager import SvnCheckoutManager
 from .svn_checkout_control import SvnCheckoutControl
 from .svn_export_control import SvnExportControl
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = [
     "SvnCheckoutManager",
     "SvnCheckoutControl", 
